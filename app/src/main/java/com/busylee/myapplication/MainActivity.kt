a83dfe07212d613478ad5330a29247fecbd73daf
busylee <busylee@yandex-team.ru>
2020-02-21 12:56:57 +0300

    init

---
+++ app/src/main/java/com/busylee/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.busylee.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val text = findViewById<EditText>(R.id.et_text)
+        findViewById<View>(R.id.btn_action).setOnClickListener {
+            startActivity(
+                Intent(this, SecondActivity::class.java).apply {
+                    putExtra(SecondActivity.EXT_TEXT, text.text.toString())
+                }
+            )
+        }
+    }
+}
